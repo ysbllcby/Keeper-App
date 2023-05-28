e88947116e31b4ca0f2f82972bd108793707f8bb
ysbllcby <ysabellacubay@gmail.com>
2023-05-28 17:49:49 -0600

    First commit.

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>My Note</h1>
+      <p>My note content</p>
+    </div>
+  );
+}
+export default Note;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Note />
+    <Footer />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
